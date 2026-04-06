555578600a9b38f8799d4dd0cb8545d5a1d4b93b
jtvrfs1343 <5150hjc@gmail.com>
2026-04-05 23:05:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("show");
 });
+// Soccer balls generator
+for(let i = 0; i < 12; i++){
+  let ball = document.createElement("div");
+  ball.className = "soccer-ball";
+
+  ball.style.left = Math.random() * 100 + "vw";
+  ball.style.animationDuration = (8 + Math.random() * 10) + "s";
+  ball.style.opacity = Math.random() * 0.2;
+
+  document.body.appendChild(ball);
+}
