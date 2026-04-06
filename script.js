7421ce75b0d85ac1708bbbe6c57ceb7af9dc1c00
jtvrfs1343 <5150hjc@gmail.com>
2026-04-05 22:30:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-
-}
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("show");
 });
-
-/* PAGE TRANSITION */
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", function(e){
-    if(this.href){
-      e.preventDefault();
-      document.body.classList.remove("show");
-      setTimeout(() => {
-        window.location = this.href;
-      }, 400);
-    }
-  });
-});
-
